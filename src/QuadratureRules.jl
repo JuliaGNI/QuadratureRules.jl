dd874e731629f383152d20d689879d947b2bd6ce
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-03-21 11:30:16 -0600

    Implement tanh-sinh quadrature.

--- src/QuadratureRules.jl
+++ src/QuadratureRules.jl
@@ -23,27 +23,31 @@ module QuadratureRules
     include("clenshaw_curtis.jl")
     include("gauss_legendre.jl")
     include("lobatto_legendre.jl")
+    include("tanh_sinh.jl")
 
     export ChebyshevQuadrature,
            GaussChebyshevQuadrature,
            LobattoChebyshevQuadrature,
            ClenshawCurtisQuadrature,
            GaussLegendreQuadrature,
-           LobattoLegendreQuadrature
+           LobattoLegendreQuadrature,
+           TanhSinhQuadrature
 
     export chebyshev_points,
            gauss_chebyshev_points,
            lobatto_chebyshev_points,
            clenshaw_curtis_points,
            gauss_legendre_points,
-           lobatto_legendre_points
+           lobatto_legendre_points,
+           tanh_sinh_points
 
     export chebyshev_nodes,
            gauss_chebyshev_nodes,
            lobatto_chebyshev_nodes,
            clenshaw_curtis_nodes,
            gauss_legendre_nodes,
-           lobatto_legendre_nodes
+           lobatto_legendre_nodes,
+           tanh_sinh_nodes
 
 
 end
